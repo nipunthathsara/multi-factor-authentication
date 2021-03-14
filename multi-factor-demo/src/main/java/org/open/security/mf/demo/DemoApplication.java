f5596d70680528798f73bccba5ecfaebac42eece
nipunthathsara <nipunthathsara@gmail.com>
2021-03-14 17:13:32 +0530

    Make springboot standalone app

---
+++ multi-factor-demo/src/main/java/org/open/security/mf/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package org.open.security.mf.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ */
+
+@SpringBootApplication(scanBasePackages = "org.open.security")
+@RestController
+public class DemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
